9d229bb459a3cc52fc0aefd95c9a79cee5011c4e
Jaysheth52 <17.comp.jay.sheth@gmail.com>
2020-01-21 13:48:59 +0530

    commit 3[C

--- solutions/practical1/Program.cs
+++ solutions/practical1/Program.cs
@@ -7,14 +7,32 @@ namespace practical1
 {
     class Program
     {
-        static void main(string[] args)
+        static void pattern1()
         {
-            for(int i=0; i<5; i++) {
-                for(int j=0; j<i; j++) 
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
                     Console.Write("*");
                 Console.WriteLine();
             }
+        }
+
+        static void pattern2()
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = i; j >= 0; j--)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            pattern1();
             Console.WriteLine();
+            pattern2();
+            Console.ReadLine();
         }
         
     }
